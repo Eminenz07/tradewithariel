dee93f338db2249659ee093a504d5da10c2969e2
Eminenz07 <eminenceadeoluwa@gmail.com>
2026-01-05 02:43:08 +0100

    Fix requirements: remove windows-only packages

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+echo "Upgrading pip..."
+python -m pip install --upgrade pip
+
+echo "Installing requirements..."
 pip install -r requirements.txt
 
+echo "Collecting static..."
 python manage.py collectstatic --no-input
+
+echo "Migrating..."
 python manage.py migrate
